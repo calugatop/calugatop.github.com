669855ef61f7db3585e20c1d267342ffbca9fc93
calugatop <calugatop@gmail.com>
2019-08-16 21:50:58 +0300

    16

---
+++ res/access.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = '1xoyn1ou';
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'groups' => array('sayhg7oh'),
+		'id' => 'p6r0fe2a',
+		'name' => 'Admin',
+		'password' => 'rv2h7ynv',
+		'email' => '',
+		'page' => 'index.html'
+	),
+	'новый пользователь' => array(
+		'groups' => array('7nhtr2as'),
+		'id' => '1y1g4xc8',
+		'name' => 'Новый пользователь',
+		'password' => '4ngdh4as',
+		'email' => '',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('p6r0fe2a');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
